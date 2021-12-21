dae0c47e02e3f7db07a99f4c9adbb3ca542a9cd7
Park Joyeong <drhamst@gmail.com>
2021-12-21 12:59:39 +0900

    Doesn't work

--- client/src/components/views/DetailVideoPage/DetailVideoPage.js
+++ client/src/components/views/DetailVideoPage/DetailVideoPage.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { List, Avatar, Typograph } from 'antd';
+import axios from 'axios';
 
-const DetailVideoPage = () => {
+const DetailVideoPage = (props) => {
+
+    const videoId = props.match.params.videoId;
+    const [video, setVideo] = useState([]);
+
+    const videoVariable = {
+        videoId: videoId
+    };
+
+    useEffect(() => {
+        axios.post('/api/video/getVideo', videoVariable)
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data.video)
+                    setVideo(response.data.video);
+                } else {
+                    alert('Failed to get video info');
+                }
+            });
+    }, []);
+
     return (
-        <div>
-            Detail Page
+        <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
+            filePath: {video.filePath}
+            <video style={{ width: '100%' }} src={`http://localhost:5000/uploads/1639969756711_movie1.avi`} controls></video>
+
+            <List.Item
+                actions={[]}
+            >
+                <List.Item.Meta
+                    avatar={<Avatar src={video.writer && video.writer.image} />}
+                    title={<a href="https://ant.design">{video.title}</a>}
+                    description={video.description}
+                />
+                <div></div>
+            </List.Item>
         </div>
     );
 };
