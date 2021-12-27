b2396a4dc470910bb2b30576807d1d7113b4a75f
Park Joyeong <drhamst@gmail.com>
2021-12-27 12:58:56 +0900

    Making comment part

--- client/src/components/views/DetailVideoPage/Sections/Comments.js
+++ client/src/components/views/DetailVideoPage/Sections/Comments.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Input } from 'antd';
+const { TextArea } = Input;
 
 function Comments(props) {
+
+    const [comment, setComment] = useState("");
+
+    const handleChange = () => {
+        setComment(e.currentTarget.value);
+    }
+
     return (
         <div>
-            Comments
+            <br />
+            <p>replies</p>
+            <hr />
+            <form style={{ display: 'flex' }} onSubmit={onSubmit}>
+                <TextArea
+                    style={{ width: '100%', borderRadius: '5px' }}
+                    onChange={handleChange}
+                    placeholder="write some comments" />
+                <br />
+                <Button style={{ width: '20%', height: '52px' }}>Submit</Button>
+            </form>
         </div>
     );
 }
