c5559334f913395b9705518bfbd0e516786ab1c0
Park Joyeong <drhamst@gmail.com>
2022-01-18 12:59:17 +0900

    Progressing lazyloading

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -10,6 +10,16 @@ const { Meta } = Card;
 
 function LandingPage() {
     const [videos, setVideos] = useState([]);
+    const imgRefs = useRef([]);
+
+    const loadReal = (img) => {
+        console.log(img)
+        console.log(img.display)
+        if (img.display != "none") {
+            img.onload = null;
+            img.src = img.getAttribute("data-src");
+        }
+    }
     useEffect(() => {
         axios.get('/api/video/getVideos')
             .then(response => {
@@ -26,7 +36,14 @@ function LandingPage() {
         return <Col lg={6} md={8} xs={24} key={index}>
             <div style={{ position: 'relative' }}>
                 <a href={`/video/${video._id}`}>
-                    <img style={{ width: '100%' }} alt="thumbnail" src={`${config.hostUrl}:5000/${video.thumbnail}`} />
+                    <img 
+                        style={{ width: '100%' }} 
+                        alt="thumbnail" 
+                        src="/img/1px.png" 
+                        data-src={`${config.hostUrl}:5000/${video.thumbnail}`}
+                        onLoad={loadReal(this)}
+                        ref={imgRefs}
+                    />
                     <div className="duration"
                         style={{
                             bottom: 0, right: 0, position: 'absolute', margin: '4px',
